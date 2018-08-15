0574792783223f6365729032274915762492b6ad
C. Aaron Cois <aaron.cois@gmail.com>
2018-08-15 11:56:08 -0600

    editing vpc ingress

--- install-pcf/aws/terraform/vpc.tf
+++ install-pcf/aws/terraform/vpc.tf
@@ -28,7 +28,13 @@ resource "aws_security_group" "nat_instance_sg" {
         from_port = 0
         to_port = 0
         protocol = -1
-        cidr_blocks = ["0.0.0.0/0"]#["${var.vpc_cidr}"]
+        cidr_blocks = ["${var.vpc_cidr}"]
+    }
+    ingress {
+        from_port = 22
+        to_port = 22
+        protocol = -1
+        cidr_blocks = ["0.0.0.0/0"]
     }
     egress {
         from_port = 0
